2c5546cf5c5f7cd84aa57f67a73ea1a9083009f5
Lance Hartung <lance@exis.io>
2016-01-07 15:57:38 -0600

    Removed experimental code

--- example/example.py
+++ example/example.py
@@ -5,13 +5,7 @@ import riffle
 
 class ExampleSession(riffle.Domain):
     def onJoin(self):
-        self.register("hello#details", self.hello)
         self.register("echo", self.echo)
-
-    def hello(self, details):
-        caller = details.get("caller", "anonymous")
-        print("Received hello from: {}".format(caller))
-        return "Hello, {}".format(caller)
 
     @riffle.want(str)
     def echo(self, msg):
